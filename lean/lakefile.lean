58133b18cb1e1fafc5b07bfb66724da428ac8641
Hanting Zhang <hantingz@usc.edu>
2025-01-10 16:45:30 -0800

    fix: Adjust `aeneas` path to `./lean` subfolder

--- lean/lakefile.lean
+++ lean/lakefile.lean
@@ -5,7 +5,7 @@ open Lake DSL
 -- require mathlib from git "https://github.com/leanprover-community/mathlib4" @ "v4.12.0"
 -- require assertCmd from git "https://github.com/pnwamk/lean4-assert-command" @ "main"
 
-require base from "../../aeneas/backends/lean"
+require base from "./aeneas/backends/lean"
 
 package «verified_fips_cryptography» where
   -- Settings applied to both builds and interactive editing
